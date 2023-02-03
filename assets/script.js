e563f372cf0a360b2ec92e6d710948abcd4f0e0e
jrob02180 <jrob02180@gmail.com>
2023-02-02 21:28:10 -0700

    started pseudocode

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
 console.log(`JavaScript file is linked`);
+// When a user clicks on a link in the header, the're directed to bottom of page to select from dropdown
+// When a user selects a city (or country?) from the dropdown they're redirected to the next page the 2nd html page 
+    // Destination is diplayed
+    // Local weather is displayed
+    // List of local common birds
+        // click on icon to play bird sounds?
+
+// At bottom of page one, list of most searched countries
+    // this will link to 2nd page
